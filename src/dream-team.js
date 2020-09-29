c6c6cfe843fe73f8ee430ab6bba9c1ba5ed103d8
alex-beaverg <a.beaverg@gmail.com>
2020-09-29 21:39:04 +0300

    docs: update task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let name;
+  let team = '';
+  if (Array.isArray(members)) {
+    for (name of members) {
+      if (typeof name === 'string') {
+              team = team + name.trim().charAt(0).toUpperCase();
+      }
+    }
+    return team.split('').sort().join('');
+  } else return false;
 };
