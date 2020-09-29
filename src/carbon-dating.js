d1bc367f5413869df1b310ed3fb2f95dda4e45fa
alex-beaverg <a.beaverg@gmail.com>
2020-09-29 20:49:33 +0300

    docs: update basic JS

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,8 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
-  let k = 0.693 / HALF_LIFE_PERIOD;
-  return Math.ceil(Math.log(sampleActivity / MODERN_ACTIVITY) / k);
+module.exports = function dateSample(sampleActivity) {  
+  if (typeof sampleActivity === 'string' && 
+      sampleActivity < 9000 && 
+      sampleActivity !== NaN &&
+      sampleActivity > 0 &&
+      Math.ceil(sampleActivity) / sampleActivity === 1) {
+    return Math.ceil(Math.log(sampleActivity / MODERN_ACTIVITY) / (0.693 / HALF_LIFE_PERIOD));
+  } else return false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {  
   let count = 0;
   for (let subArr of matrix) {
     for (let elem of subArr) {
