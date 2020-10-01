d2891381c21af91112c8127d0d5eca89c4ac0db0
alex-beaverg <a.beaverg@gmail.com>
2020-10-02 01:46:37 +0300

    docs: update task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turn = 2 ** disksNumber - 1;
+  let second = Math.floor((turn / turnsSpeed) * 60 * 60);
+  let obj = {
+    turns: turn, 
+    seconds: second
+  };  
+  return obj;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {  
+  if (!Array.isArray(arr)) throw Error;
+  let arrRes=[];
+  let flag = false;
+  let flag2 = false;
+  let flag3 = false;
+  for (let item of arr) {    
+    if (flag === true) {
+      flag = false;      
+      continue;
+    }
+    if (flag2 === true) {
+      flag2 = false;
+      arrRes.push(item);
+      arrRes.push(item);
+      continue;
+    }
+    switch (item) {
+      case '--discard-next':
+        flag = true;
+        flag3 = true;
+        break;
+      case '--discard-prev':
+        if (arrRes.length === 0) continue;
+        if (flag3 === true) {
+          flag3 = false;
+          continue;
+        }
+        arrRes.pop();
+        break;
+      case '--double-next':
+        flag2 = true;
+        break;
+      case '--double-prev':
+        if (arrRes.length === 0) continue;
+        if (flag3 === true) {
+          flag3 = false;
+          continue;
+        }
+        arrRes.push(arrRes[arrRes.length - 1]);
+        break;
+      default:
+        arrRes.push(item);
+    }    
+  }  
+  return arrRes;
+}
