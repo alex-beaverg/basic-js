26578bf38988c82f9fcb6b5aa9ec88c5b387e62e
alex-beaverg <a.beaverg@gmail.com>
2020-10-04 05:09:16 +0300

    docs: update task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: '',    
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {    
+      this.chain += '~~( ' + String(value) + ' )';
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || typeof position != 'number') {
+      this.chain = '';
+      throw Error;
+    }
+    let chainArr = this.chain.split('~~');
+    chainArr.shift();
+    if (position >= chainArr.length) {
+      this.chain = '';
+      throw Error;
+    }
+    let count = 1;
+    let chain2 = '';
+    for (item of chainArr) {      
+      if (count != position) chain2 += '~~' + item;
+      count++;
+    }
+    this.chain = chain2;
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chainArr = this.chain.split('~~');
+    chainArr.shift();    
+    let count = 1;
+    let chain2 = '';
+    for (item of chainArr) {      
+      chain2 = item + '~~' + chain2;
+      count++;
+    }
+    if (chainArr.length === 0) {
+      this.chain = '';
+    } else {
+      this.chain = '~~' + chain2.substr(0, chain2.length - 2);
+    }
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.substr(2);
+    this.chain = '';
+    return result;
   }
 };
 
