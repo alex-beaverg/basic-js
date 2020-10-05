6ebb8b76c27861efe5e4a0a9517257b77632d732
alex-beaverg <a.beaverg@gmail.com>
2020-10-05 23:53:06 +0300

    docs: task is done!

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,96 @@
 const CustomError = require("../extensions/custom-error");
 
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+class VigenereCipheringMachine {    
+  constructor(f) {
+    this.f = f;      
+  }  
+  encrypt(message, key) {    
+    if (message === undefined || key === undefined) throw Error;
+    let result = '';
+    let alpha = [];
+    let ab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let abeth = 'ZABCDEFGHIJKLMNOPQRSTUVWXY';
+    let abethNew = '';
+    for (let i = 0; i < abeth.length; i++) {
+        alpha.push(abeth.charAt(i));
+      alpha[i] = [];
+      abethNew = abeth.slice(i+1) + abeth.slice(0, i+1);
+        for (let j = 0; j < abeth.length; j++) {       
+        alpha[i].push(abethNew.charAt(j));
+      }
+    }   
+    let password = '';
+    let key0 = '';
+    for (let i = 0; i < message.length; i++) {
+        key0 += key;
+    }
+    key0 = key0.toUpperCase().slice(0, message.length);
+    for (let i = 0; i < message.length; i++) {
+        if (message.toUpperCase().codePointAt(i) > 64 && message.toUpperCase().codePointAt(i) < 91) {   
+        password += key0.charAt(i);
+      } else {
+        password += message.charAt(i);
+        key0 = ' ' + key0;
+      }
+    }   
+    for (let i = 0; i < message.length; i++) {
+        if (message.toUpperCase().codePointAt(i) > 64 && message.toUpperCase().codePointAt(i) < 91) {
+                result += alpha[ab.indexOf(String(message.toUpperCase().charAt(i)))][ab.indexOf(String(password.toUpperCase().charAt(i)))];
+      } else {
+        result += message.charAt(i);
+      }
+    }   
+    if (this.f === false) {
+      let revResult = '';
+      for (let char of result) {
+        revResult = char + revResult;
+      }
+      return revResult;
+    } else return result;
+  }  
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw Error;
+    let result = '';
+    let alpha = [];
+    let ab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let abeth = 'ZABCDEFGHIJKLMNOPQRSTUVWXY';
+    let abethNew = '';
+    for (let i = 0; i < abeth.length; i++) {
+        alpha.push(abeth.charAt(i));
+      alpha[i] = [];
+      abethNew = abeth.slice(i+1) + abeth.slice(0, i+1);
+        for (let j = 0; j < abeth.length; j++) {       
+        alpha[i].push(abethNew.charAt(j));
+      }
+    }   
+    let password = '';
+    let key0 = '';
+    for (let i = 0; i < message.length; i++) {
+        key0 += key;
+    }
+    key0 = key0.toUpperCase().slice(0, message.length);
+    for (let i = 0; i < message.length; i++) {
+        if (message.toUpperCase().codePointAt(i) > 64 && message.toUpperCase().codePointAt(i) < 91) {   
+        password += key0.charAt(i);
+      } else {
+        password += message.charAt(i);
+        key0 = ' ' + key0;
+      }
+    }   
+    for (let i = 0; i < message.length; i++) {
+        if (message.toUpperCase().codePointAt(i) > 64 && message.toUpperCase().codePointAt(i) < 91) {
+          result += alpha[0][alpha[ab.indexOf(String(password.toUpperCase().charAt(i)))].indexOf(String(message.toUpperCase().charAt(i)))];
+      } else {
+        result += message.charAt(i);
+      }
+    }   
+    if (this.f === false) {
+      let revResult = '';
+      for (let char of result) {
+        revResult = char + revResult;
+      }
+      return revResult;
+    } else return result;
   }
 }
 
