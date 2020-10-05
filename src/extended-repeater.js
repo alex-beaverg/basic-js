cb84c0425e431f8642e378a9ffe1f7ca04ee183b
alex-beaverg <a.beaverg@gmail.com>
2020-10-05 21:46:59 +0300

    docs: update task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function(str, options = { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator }) {
+  if (options.separator === undefined) options.separator = '+';
+  if (options.additionSeparator === undefined) options.additionSeparator = '|';
+  let result = '';
+  let halfResult = ''; 
+  if (options.addition === undefined) {
+  	halfResult = '';  	
+  } else {
+  	for (let i = 0; i < options.additionRepeatTimes; i++) {
+    	if (i === 0) halfResult = String(options.addition);
+    	else halfResult += options.additionSeparator + String(options.addition);
+  	}
+  }  
+  if (options.additionRepeatTimes === undefined && options.addition !== undefined) halfResult = String(options.addition);  
+  for (let i = 0; i < options.repeatTimes; i++) {
+    if (i === 0) result = String(str) + halfResult;
+    else result += options.separator + String(str) + halfResult;
+  }
+  if (options.repeatTimes === undefined) result = String(str) + halfResult;
+  return result;
 };
   
